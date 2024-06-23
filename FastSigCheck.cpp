293e19baf6508cbb271d012e91b4249ef7d076ca
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-06-23 13:13:14 -0700

    Add additional invalid signature return codes. Add error messages for debugging.

--- FastSigCheck.cpp
+++ FastSigCheck.cpp
@@ -2,8 +2,31 @@
 #include <wintrust.h>
 #include <softpub.h>
 #include <iostream>
+#include <unordered_map>
 
 #pragma comment(lib, "wintrust.lib")
+
+std::unordered_map<LONG, std::wstring> errorMessages = {
+    {TRUST_E_NOSIGNATURE, L"The file is not signed."},
+    {TRUST_E_BAD_DIGEST, L"The digital signature of the object did not verify."},
+    {CERT_E_CHAINING, L"A chain of certificates could not be created."},
+    {CERT_E_CRITICAL, L"A certificate contains an unknown extension that is marked 'critical.'"},
+    {CERT_E_INVALID_NAME, L"The certificate has a name that is not valid. The name is either not included in the permitted list or is explicitly excluded."},
+    {CERT_E_INVALID_POLICY, L"The certificate has a policy that is not valid."},
+    {CERT_E_ISSUERCHAINING, L"A parent of a given certificate in fact did not issue that child certificate."},
+    {CERT_E_MALFORMED, L"A certificate is missing or has an empty value for an important field, such as a subject or issuer name."},
+    {CERT_E_PATHLENCONST, L"A path length constraint in the certification chain has been violated."},
+    {CERT_E_UNTRUSTEDCA, L"A certification chain processed correctly, but one of the CA certificates is not trusted by the policy provider."},
+    {CRYPT_E_NO_REVOCATION_CHECK, L"The revocation function was unable to check revocation for the certificate."},
+    {TRUST_E_BASIC_CONSTRAINTS, L"The basic constraint extension of a certificate has not been observed."},
+    {TRUST_E_CERT_SIGNATURE, L"The signature of the certificate cannot be verified."},
+    {TRUST_E_COUNTER_SIGNER, L"One of the counter signatures was not valid."},
+    {TRUST_E_EXPLICIT_DISTRUST, L"The certificate was explicitly marked as untrusted by the user."},
+    {TRUST_E_FINANCIAL_CRITERIA, L"The certificate does not meet or contain the Authenticode financial extensions."},
+    {TRUST_E_NO_SIGNER_CERT, L"The certificate for the signer of the message is not valid or not found."},
+    {TRUST_E_SYSTEM_ERROR, L"A system-level error occurred while verifying trust."},
+    {TRUST_E_TIME_STAMP, L"The time stamp signature or certificate could not be verified or is malformed."}
+};
 
 int VerifyFileSignature(LPCWSTR filePath, bool debug)
 {
@@ -37,7 +60,13 @@ int VerifyFileSignature(LPCWSTR filePath, bool debug)
 
     if (debug)
     {
-        std::wcout << L"WinVerifyTrust returned: " << std::hex << status << std::endl;
+        std::wcout << L"WinVerifyTrust returned: " << std::hex << status;
+        auto it = errorMessages.find(status);
+        if (it != errorMessages.end())
+        {
+            std::wcout << L" (" << it->second << L")";
+        }
+        std::wcout << std::endl;
     }
 
     return status;
@@ -76,7 +105,23 @@ int wmain(int argc, wchar_t* argv[])
         return 1;
     case TRUST_E_BAD_DIGEST:
     case CERT_E_CHAINING:
-        std::wcout << L"The file's signature is invalid." << std::endl;
+    case CERT_E_CRITICAL:
+    case CERT_E_INVALID_NAME:
+    case CERT_E_INVALID_POLICY:
+    case CERT_E_ISSUERCHAINING:
+    case CERT_E_MALFORMED:
+    case CERT_E_PATHLENCONST:
+    case CERT_E_UNTRUSTEDCA:
+    case CRYPT_E_NO_REVOCATION_CHECK:
+    case TRUST_E_BASIC_CONSTRAINTS:
+    case TRUST_E_CERT_SIGNATURE:
+    case TRUST_E_COUNTER_SIGNER:
+    case TRUST_E_EXPLICIT_DISTRUST:
+    case TRUST_E_FINANCIAL_CRITERIA:
+    case TRUST_E_NO_SIGNER_CERT:
+    case TRUST_E_SYSTEM_ERROR:
+    case TRUST_E_TIME_STAMP:
+        std::wcout << errorMessages[result] << std::endl;
         return 2;
     default:
         std::wcout << L"An error occurred: " << std::hex << result << std::endl;
